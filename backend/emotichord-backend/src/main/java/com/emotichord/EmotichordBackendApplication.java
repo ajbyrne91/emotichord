a2b0156d349b78e362f27b810e5b86391622b3f8
Alex Byrne <alexbyrne@Mac.lan>
2025-11-24 12:37:36 +0000

    Spring Boot backend setup complete

---
+++ backend/emotichord-backend/src/main/java/com/emotichord/EmotichordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emotichord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotichordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotichordBackendApplication.class, args);
+	}
+
+}
---
+++ backend/emotichord-backend/src/test/java/com/emotichord/EmotichordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotichord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotichordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
